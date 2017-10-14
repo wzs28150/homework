79c6a047dea7b3c18c57e735bdc309a146fc24de
wzs28150 <wz666s@163.com>
2017-10-14 08:26:10 +0800

    add

--- 201710/wap.sanlian.com/src/scripts/lib/index.js
+++ 201710/wap.sanlian.com/src/scripts/lib/index.js
@@ -1,6 +1,5 @@
-define(['domReady', 'debouncedresize'], function (domReady, debouncedresize) {
-  var initModule, banner, headerh = $('header').height(),
-    bannerpic, news;
+define(['domReady'], function (domReady) {
+  var initModule, banner, headerh = $('header').height();
   banner = function () {
     var bigswiper = new Swiper('.index .banner.swiper-container', {
       autoplay: 3000,
@@ -12,19 +11,13 @@ define(['domReady', 'debouncedresize'], function (domReady, debouncedresize) {
       prevButton: '.index .banner .swiper-button-prev',
     });
   };
-  bannerpic = function () {
-   
-  }
+
 
   initModule = function (callbak) {
     domReady(function () {
       callbak(0);
       require(["swiper"], function (swiper) {
         banner();
-      });
-      bannerpic();
-      $(window).on('debouncedresize', function () {
-        bannerpic();
       });
     });
   };
