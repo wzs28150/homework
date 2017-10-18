466c4c580884ddc17797137f7c45a997807d5e55
wzs28150 <wz666s@163.com>
2017-10-18 10:15:58 +0800

    add

--- 201710/wap.sanlian.com/src/scripts/lib/hr.js
+++ 201710/wap.sanlian.com/src/scripts/lib/hr.js
@@ -1,12 +1,20 @@
 define(['debug'], function (debug) {
   var initModule, hr_index, debug = debug.initModule;
-  hr_index = function (callbak){
+  hr_index = function (callbak) {
     debug('用人理念已加载');
     callbak(0);
   };
   hr_zhaomu = function (callbak) {
     debug('人才招募已加载');
     callbak(1);
+    $('.hr .hr-list .item').click(function () {
+      var des = $(this).find('.des');
+      if (des.is(":hidden")) {
+        des.show();
+      } else {
+        des.hide();
+      }
+    });
   };
   initModule = function (callbak) {
     callbak(7);
