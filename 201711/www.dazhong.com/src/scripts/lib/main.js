e1ae1ee9591a3d74e17987d3093c31709056f6d8
wzs28150 <wz666s@163.com>
2017-11-01 22:17:35 +0800

    add

--- 201711/www.dazhong.com/src/scripts/lib/main.js
+++ 201711/www.dazhong.com/src/scripts/lib/main.js
@@ -1,134 +1,112 @@
-define(['pajax', 'debouncedresize', 'router', 'smoothscroll', 'scroll'], function(pajax, debouncedresize, router, smoothscroll, scroll) {
-  var initModule, animated_contents, minheight, alertinfo, exists, backtotop;
-  /**
-   * 判断dom是否存在
-   * @param  {[type]} selector [description]
-   * @return {[type]}          [description]
-   */
-  exists = function(selector) {
-    "use strict";
-    return (jQuery(selector).length > 0);
-  };
+var initModule, animated_contents, minheight, alertinfo, exists, backtotop, fenleishow, loadtemp;
+/**
+ * 判断dom是否存在
+ * @param  {[type]} selector [description]
+ * @return {[type]}          [description]
+ */
+exists = function(selector) {
+  "use strict";
+  return (jQuery(selector).length > 0);
+};
 
-  /**
-   * 动画设置
-   * @return {[type]} [description]
-   */
-  animated_contents = function() {
-    "use strict";
-    if (exists('.animate-element')) {
+/**
+ * 动画设置
+ * @return {[type]} [description]
+ */
+animated_contents = function() {
+  "use strict";
+  if (exists('.animate-element')) {
 
-      $(".animate-element:in-viewport").each(function(i) {
+    $(".animate-element:in-viewport").each(function(i) {
 
-        var $this = $(this);
-        if (!$this.hasClass('in-viewport')) {
-          setTimeout(function() {
-            $this.addClass('in-viewport');
-          }, 100 * i);
-        }
-      });
-    }
-  }
-
-
-  /**
-   * 弹窗
-   * @param  {[type]} info [description]
-   * @param  {[type]} aurl [description]
-   * @return {[type]}      [description]
-   */
-  alertinfo = function(info, aurl) {
-    $('.alert_player').fadeIn();
-    $('.alert_player .info').html(info);
-    if (aurl === undefined) {} else {
-      $('.alert_player .queren_btn,.alert_player .bg').click(function() {
-        location.href = aurl;
-      })
-    }
-
-  };
-
-  backtotop = function() {
-    $("#backtotop").on("click", function() {
-      $('body,html').animate({
-        scrollTop: 0
-      }, 1000);
-      return false;
-    });
-  }
-  minheight = function() {
-    var hh = $('header').height();
-    var fh = $('footer').height();
-    var wh = $(window).height();
-    var nh = $('nav').height();
-    var mh;
-    if (nh > wh) {
-      mh = nh - fh - hh;
-    } else {
-      mh = $(window).height() - fh - hh;
-    }
-    $('article').css('min-height', mh + 'px');
-  }
-
-
-  initModule = function() {
-    //必须加载模块
-    router.initModule('article', false);
-    var $ww = $(window).width();
-    //scroll.initModule('body');
-    scroll.initModule('.scroller');
-
-    minheight();
-    backtotop();
-    //parallaxan();
-    require(["viewport"], function(viewport) {
-      animated_contents();
-      $(window).scroll(function(event) {
-        /* Act on the event */
-        animated_contents();
-      });
-    });
-
-    $(window).on('debouncedresize', function() {
-      minheight();
-    });
-
-    pajax.initModule('main', function() {}, function(targetelement, state) {
-      router.initModule('article', state);
-      backtotop();
-      minheight();
-      scroll.initModule('.scroller');
-      //scroll.resetsize('body');
-      require(["viewport"], function(viewport) {
-        animated_contents();
-        $(window).scroll(function(event) {
-          /* Act on the event */
-          animated_contents();
-        });
-      });
-      $(window).on('debouncedresize', function() {
-        minheight();
-      });
-      if (targetelement) {
-        //console.log(targetelement);
+      var $this = $(this);
+      if (!$this.hasClass('in-viewport')) {
         setTimeout(function() {
-          //console.log($('#' + targetelement).offset().top);
-          $.smoothScroll({
-            offset: -($('header').height()),
-            speed: 500,
-            scrollTarget: '#' + targetelement
-          });
-        }, 500);
+          $this.addClass('in-viewport');
+        }, 100 * i);
       }
     });
+  }
+}
 
-    $('.alert_player .bg,.alert_player .queren_btn').click(function() {
-      $('.alert_player').fadeOut();
+
+/**
+ * 弹窗
+ * @param  {[type]} info [description]
+ * @param  {[type]} aurl [description]
+ * @return {[type]}      [description]
+ */
+alertinfo = function(info, aurl) {
+  $('.alert_player').fadeIn();
+  $('.alert_player .info').html(info);
+  if (aurl === undefined) {} else {
+    $('.alert_player .queren_btn,.alert_player .bg').click(function() {
+      location.href = aurl;
+    })
+  }
+
+};
+
+backtotop = function() {
+  $("#backtotop").on("click", function() {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 1000);
+    return false;
+  });
+}
+minheight = function() {
+  var hh = $('header').height();
+  var fh = $('footer').height();
+  var fqh = $('.footer-quick').height();
+  var wh = $(window).height();
+  var nh = $('nav').height();
+  var mh;
+  mh = $(window).height() - fh - hh - fqh;
+  if (mh < 460) {
+    mh = 460;
+  }
+  $('article').css('min-height', mh + 'px');
+}
+
+fenleishow = function() {
+  $('.fenlei>ul').css({
+    "display": "block"
+  })
+}
+loadtemp = function() {
+  if (exists('.jiazai')) {
+    $('.jiazai').each(function(index, el) {
+      var a = $(this).attr('id');
+      var callback = $(this).attr('data-callback');
+      $.get("/" + a + ".html",
+        function(tempdata) {
+          $('#' + a).html(tempdata);
+          var myeval = eval;
+          myeval(callback + '();');
+        });
     });
-  };
 
-  return {
-    initModule: initModule,
-    alertinfo: alertinfo
-  };
-})
+  }
+}
+initModule = function() {
+  var $ww = $(window).width();
+  $('.scroller').scrollbar();
+  minheight();
+  backtotop();
+  loadtemp();
+  animated_contents();
+  $(window).scroll(function(event) {
+    animated_contents();
+  });
+  $(window).on('debouncedresize', function() {
+    minheight();
+  });
+
+  $('.alert_player .bg,.alert_player .queren_btn').click(function() {
+    $('.alert_player').fadeOut();
+  });
+};
+$(document).ready(function() {
+  initModule();
+});
