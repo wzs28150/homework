e2556773a53420ba6bef721366073b946a90fe7d
wzs28150 <wz666s@163.com>
2017-11-02 22:14:10 +0800

    add

--- 201711/www.dazhong.com/buildconfig.js
+++ 201711/www.dazhong.com/buildconfig.js
@@ -2,38 +2,7 @@
   appDir: './',
   baseUrl: './src/scripts/',
   dir: './dist',
-  modules: [{
-    name: 'me'
-  }],
-  fileExclusionRegExp: /^(r|buildconfig)\.js|.*\.scss|.*\.bat|.*\.svg$/,
+  fileExclusionRegExp: /^(r|buildconfig)\.js|.*\.scss|.*\.bat$/,
   optimizeCss: 'standard',
-  removeCombined: true,
-  shim: {
-    'pajax': ['pjax'],
-    //'laydate': ['laydate'],
-    //'progress': ['swiper'],
-
-    'scroll': ['scrollbar']
-  },
-  paths: {
-    domReady: './domReady',
-    main: './lib/main',
-    pjax: './package/core/pajax/jquery.pjax',
-    pajax: './package/core/pajax/pajax',
-    router: './package/core/router/router',
-    debug: './package/core/debug/debug',
-    alertinfo: './package/core/alertinfo/alertinfo',
-    css: './package/core/css/css',
-    async: './package/core/async/async',
-    viewport: './package/viewport/jquery.viewport',
-    smoothscroll: './package/scroll/jquery.smooth-scroll.min',
-    scrollbar: './package/scroll/scrollbar',
-    scroll: './package/scroll/scroll',
-    aspectratio_responsive: './package/media/aspectratio_responsive',
-    swiper: './package/swiper/v3/swiper.min',
-    debouncedresize: './package/compat/jquery.debouncedresize',
-    litebox: './package/litebox/js/litebox.min',
-    loadmore: './package/loadmore/loadmore',
-    
-  }
+  removeCombined: true
 })
