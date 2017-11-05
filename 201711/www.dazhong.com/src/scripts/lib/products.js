f668b8ca54c6567ba3f94ccf1c6c9f5c47477619
wzs28150 <wz666s@163.com>
2017-11-05 15:40:15 +0800

    add

--- 201711/www.dazhong.com/src/scripts/lib/products.js
+++ 201711/www.dazhong.com/src/scripts/lib/products.js
@@ -1,10 +1,21 @@
-var brandModule, banner, new_pro, hot_pro;
+var brandModule, banner, multiple, tiaojiao;
 
-
+multiple = function() {
+  $('.tiaojiao-sort .sl .sl-list .ctrl a.morecheck').click(function (e) { 
+    e.preventDefault();
+    var sl = $(this).parent('.ctrl').parent('.sl-list').parent('.sl').toggleClass('multiple');
+  });
+}
+tiaojiao = function() {
+  $('.tiaojiao-sort .sl').each(function(i){
+    console.log(i)
+  })
+}
 indexModule = function() {
-  //设置导航选中
-  navactive(0);
-
+    //设置导航选中
+    navactive(0);
+    tiaojiao();
+    multiple();
 };
 
 indexModule();
