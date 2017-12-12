259c112a35f4563f3cb3ae8562f4c95914534084
wzs28150 <wz666s@163.com>
2017-12-12 13:53:14 +0800

    add

--- 201712/www.minjian.com/src/scripts/lib/index.js
+++ 201712/www.minjian.com/src/scripts/lib/index.js
@@ -1,4 +1,4 @@
-var indexModule, notice;
+var indexModule, notice, newshuadong, newspic;
 //首页banner
 notice = function() {
   var bigswiper = new Swiper('.index .notice .swiper-container', {
@@ -9,12 +9,33 @@ notice = function() {
   });
 };
 
-
+newshuadong = function() {
+  var newshuadongswiper = new Swiper('.index .news .news-huadong .swiper-container', {
+    speed: 1000
+  });
+  $('body').off('click', '.index .news .news-huadong .news-huadong-nav a').on('click', '.index .news .news-huadong .news-huadong-nav a', function(e) {
+    var i = $(this).index();
+    $('.index .news .news-huadong .news-huadong-nav a').removeClass('on');
+    $(this).addClass('on');
+    newshuadongswiper.slideTo(i, 1000, false);
+  });
+}
+newspic = function() {
+  var newspicswiper = new Swiper('.index .news .pic', {
+    autoplay: 3000,
+    speed: 1000,
+    loop: true,
+    pagination: '.index .news .pic .swiper-pagination',
+    paginationClickable: true,
+  });
+}
 indexModule = function() {
   //设置导航选中
   navactive(0);
   //banner切换
   notice();
+  newshuadong();
+  newspic();
 };
 
 indexModule();
