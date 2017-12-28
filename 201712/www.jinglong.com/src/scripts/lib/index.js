19fad0b50beba424ebaf552f4514f39d59fc7664
wzs28150 <wz666s@163.com>
2017-12-28 16:06:20 +0800

    add

--- 201712/www.jinglong.com/src/scripts/lib/index.js
+++ 201712/www.jinglong.com/src/scripts/lib/index.js
@@ -1,41 +1,54 @@
-var indexModule, notice, newshuadong, newspic;
+var indexModule, bannerpicchange, banner;
 //首页banner
-notice = function() {
-  var bigswiper = new Swiper('.index .notice .swiper-container', {
+banner = function() {
+  var bigswiper = new Swiper('.banner', {
     autoplay: 3000,
     speed: 1000,
     loop: true,
-    slidesPerView: 'auto'
+    pagination: '.banner .swiper-pagination',
+    paginationClickable: true,
+    onInit: function(swiper) {
+      //Swiper初始化了
+      //alert(swiper.activeIndex);提示Swiper的当前索引
+      if ($(window).width() < 980) {
+        bannerpicchange('wap');
+      } else {
+        bannerpicchange('pc');
+      }
+    }
   });
 };
 
-newshuadong = function() {
-  var newshuadongswiper = new Swiper('.index .news .news-huadong .swiper-container', {
-    speed: 1000
-  });
-  $('body').off('click', '.index .news .news-huadong .news-huadong-nav a').on('click', '.index .news .news-huadong .news-huadong-nav a', function(e) {
-    var i = $(this).index();
-    $('.index .news .news-huadong .news-huadong-nav a').removeClass('on');
-    $(this).addClass('on');
-    newshuadongswiper.slideTo(i, 1000, false);
+bannerpicchange = function(type) {
+  $('.banner .swiper-slide').each(function(index, el) {
+    if (type == 'wap') {
+      $(this).css('background-image', 'url(' + $(this).attr('data-wap-img') + ')');
+    } else {
+      $(this).css('background-image', 'url(' + $(this).attr('data-pc-img') + ')');
+    }
   });
 }
-newspic = function() {
-  var newspicswiper = new Swiper('.index .news .pic', {
-    autoplay: 3000,
-    speed: 1000,
-    loop: true,
-    pagination: '.index .news .pic .swiper-pagination',
-    paginationClickable: true,
-  });
-}
+
 indexModule = function() {
   //设置导航选中
   navactive(0);
   //banner切换
-  notice();
-  newshuadong();
-  newspic();
+  banner();
+
+  $(window).off("debouncedresize").on("debouncedresize", function(event) {
+    // Your event handler code goes here.
+    if ($(window).width() < 980) {
+      bannerpicchange('wap');
+    } else {
+      bannerpicchange('pc');
+    }
+  });
+  if ($(window).width() < 980) {
+    bannerpicchange('wap');
+  } else {
+    bannerpicchange('pc');
+  }
+
 };
 
 indexModule();
